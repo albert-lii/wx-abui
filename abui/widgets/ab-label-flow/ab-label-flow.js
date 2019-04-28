fc695d4d7233e1fc2f0e677394feb9c4658ca5e0
albertlii <albertlii@163.com>
2019-04-28 18:18:53 +0800

    尝试使用slot优化组件

--- abui/widgets/ab-label-flow/ab-label-flow.js
+++ abui/widgets/ab-label-flow/ab-label-flow.js
@@ -8,13 +8,17 @@ Component({
     }
   },
 
+  options: {
+    multipleSlots: true
+  },
+
   methods: {
     /**
      * 标签点击监听
      */
-    _labelTap:function(e){
+    _labelTap: function(e) {
       console.log(e)
-      this.triggerEvent('labeltap',e);
+      this.triggerEvent('labeltap', e);
     }
   }
 })
